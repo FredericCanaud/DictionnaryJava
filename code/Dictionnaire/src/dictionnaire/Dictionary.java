30ab3241bb3d6903f216650f72929e4695899924
ThomasCampredon <thomas.campredon@etu.unilim.fr>
2021-04-16 22:18:17 +0200

    refactor exercice 3

--- code/Dictionnaire/src/dictionnaire/Dictionary.java
+++ code/Dictionnaire/src/dictionnaire/Dictionary.java
@@ -1,29 +1,40 @@
 package dictionnaire;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Dictionary {
 	private String name;
-	private HashMap<String, String> translations;
+	private HashMap<String, ArrayList<String>> translations;
 	
 	public Dictionary(String name) {
 		this.name = name;
-		this.translations = new HashMap<String, String>();
+		this.translations = new HashMap<String, ArrayList<String>>();
 	}
 
 	public String getName() {
 		return name;
 	}
 	
-	public HashMap<String, String> getTranslations(){
+	public HashMap<String, ArrayList<String>> getTranslations(){
 		return this.translations;
 	}
 	
 	public void addTranslation(String word, String translation) {
-		this.translations.put(word, translation);
+		if (null == this.translations.get(word)) {
+			this.translations.put(word, new ArrayList<String>());
+		}
+		this.translations.get(word).add(translation);
 	}
 	
 	public String getTranslation(String word) {
+		if (null == this.translations.get(word)) {
+			return null;
+		}
+		return this.translations.get(word).get(0);
+	}
+
+	public ArrayList<String> getMultipleTranslations(String word) {
 		return this.translations.get(word);
 	}
 }
--- code/Dictionnaire/src/dictionnaire/DictionaryTest.java
+++ code/Dictionnaire/src/dictionnaire/DictionaryTest.java
@@ -1,6 +1,7 @@
 package dictionnaire;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -27,5 +28,21 @@ public class DictionaryTest {
 	public void ajoutDeTraductionDonneTraduction() {
 		this.dico.addTranslation("oui", "yes");
 		assertEquals("yes", this.dico.getTranslation("oui"));
+	}
+	
+	@Test
+	public void pasDeTraductionDonneNull() {
+		assertEquals(null, this.dico.getTranslation("oui"));
+	}
+	
+	@Test
+	public void multipleTraduction() {
+		this.dico.addTranslation("oui", "yes");
+		this.dico.addTranslation("oui", "yeah");
+		this.dico.addTranslation("oui", "yop");
+		
+		assertTrue(this.dico.getMultipleTranslations("oui").contains("yeah"));
+		assertTrue(this.dico.getMultipleTranslations("oui").contains("yes"));
+		assertTrue(this.dico.getMultipleTranslations("oui").contains("yop"));
 	}
 } 
