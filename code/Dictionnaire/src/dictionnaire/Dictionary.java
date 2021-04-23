fea2f2a88f2d48fdcfde88e057ef261554706766
ThomasCampredon <thomas.campredon@etu.unilim.fr>
2021-04-23 19:01:10 +0200

    commentaire

--- code/Dictionnaire/src/dictionnaire/Dictionary.java
+++ code/Dictionnaire/src/dictionnaire/Dictionary.java
@@ -6,24 +6,36 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Dictionary {
-	private String name;
-	private HashMap<String, ArrayList<String>> translations;
-	private HashMap<String, ArrayList<String>> reverseTranslations;
-	
+	private String name; // nom du dictionnaire
+	private HashMap<String, ArrayList<String>> translations; // HashMap des traductions
+	private HashMap<String, ArrayList<String>> reverseTranslations; // HashMap des traductions inversées
+
+	//////////////////////
+	//// constructeur ////
+	//////////////////////
+
+	// constructeur de base
 	public Dictionary(String name) {
 		this.name = name;
 		this.translations = new HashMap<String, ArrayList<String>>();
 		this.reverseTranslations = new HashMap<String, ArrayList<String>>();
 	}
 
-	public String getName() {
-		return name;
+	// constructeur avec le fichier de traduction
+	public Dictionary(BufferedReader bufferedReader) throws IOException {
+		FileParser fileParser = new FileParser();
+		Dictionary result = fileParser.readFile(bufferedReader);
+		this.name = result.name;
+		this.translations = result.translations;
+		this.reverseTranslations = result.reverseTranslations;
 	}
+
 	
-	public HashMap<String, ArrayList<String>> getTranslations(){
-		return this.translations;
-	}
-	
+	//////////////////////////////
+	//// AJOUT DE TRADUCTIONS ////
+	//////////////////////////////
+
+	// ajout de traduction
 	public void addTranslation(String word, String translation) {
 		if (null == this.translations.get(word)) {
 			this.translations.put(word, new ArrayList<String>());
@@ -31,14 +43,29 @@ public class Dictionary {
 		this.translations.get(word).add(translation);
 		this.addReverseTranslation(word, translation);
 	}
-	
-	public void addReverseTranslation(String word, String translation) {
+
+	// ajout de traduction inversée
+	private void addReverseTranslation(String word, String translation) {
 		if (null == this.reverseTranslations.get(translation)) {
 			this.reverseTranslations.put(translation, new ArrayList<String>());
 		}
 		this.reverseTranslations.get(translation).add(word);
 	}
+
 	
+	///////////////////
+	///// GETTERS /////
+	///////////////////
+
+	public String getName() {
+		return name;
+	}
+
+	public HashMap<String, ArrayList<String>> getTranslations() {
+		return this.translations;
+	}
+
+	// traduction simple
 	public String getTranslation(String word) throws TranslationNotFoundException {
 		if (null == this.translations.get(word)) {
 			throw new TranslationNotFoundException();
@@ -46,15 +73,15 @@ public class Dictionary {
 		return this.translations.get(word).get(0);
 	}
 
+	// traductions multiples
 	public ArrayList<String> getMultipleTranslations(String word) throws TranslationNotFoundException {
 		if (null == this.translations.get(word)) {
 			throw new TranslationNotFoundException();
 		}
 		return this.translations.get(word);
 	}
-	
-	
-	
+
+	// traduction inversée
 	public String getReverseTranslation(String word) throws TranslationNotFoundException {
 		if (null == this.reverseTranslations.get(word)) {
 			throw new TranslationNotFoundException();
@@ -62,18 +89,11 @@ public class Dictionary {
 		return this.reverseTranslations.get(word).get(0);
 	}
 
+	// traductions inversées multiples
 	public ArrayList<String> getMultipleReverseTranslations(String word) throws TranslationNotFoundException {
 		if (null == this.reverseTranslations.get(word)) {
 			throw new TranslationNotFoundException();
 		}
 		return this.reverseTranslations.get(word);
-	}
-
-	public Dictionary(BufferedReader bufferedReader) throws IOException {
-		FileParser fileParser = new FileParser();
-		Dictionary result = fileParser.readFile(bufferedReader);
-		this.name = result.name;
-		this.translations = result.translations;
-		this.reverseTranslations = result.reverseTranslations;
 	}
 }
